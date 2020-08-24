9bf94d7d934f3faa0493c0642ab9e179d6168fb3
zhaochen0623 <429702531@qq.com>
2020-08-24 20:14:55 +0800

    spring 初始项目 增加eureka注册模块 将maven项目改造成父子工程

---
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName EurekaApplication
+ * @Author ASUS
+ * @Date 2020-08-24 20:09
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
