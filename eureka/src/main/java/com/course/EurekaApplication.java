74c688ed031f3687aabf28fb0a27ec3c4e76f1c3
zhaochen0623 <429702531@qq.com>
2020-08-25 10:41:33 +0800

    spring 初始项目 增加eureka注册模块 将maven项目改造成父子工程 优化日志显示

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,7 +1,11 @@
 package com.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 /**
  * @ClassName EurekaApplication
@@ -10,8 +14,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  **/
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class,args);
+    private static final Logger LOG= LoggerFactory.getLogger(EurekaApplication.class);
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(EurekaApplication.class,args);
+//    }
+    public static void main(String[] args){
+        SpringApplication springApplication=new SpringApplication(EurekaApplication.class);
+        Environment environment=springApplication.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("Eureka地址:\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
     }
 }
