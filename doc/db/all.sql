ee27489afb8072f4602402cd26f6479c007045ce
zhaochen0623 <429702531@qq.com>
2020-08-27 19:20:40 +0800

    1.新建大章表,生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,28 @@
+
+
+
+
+
+
+
+
+
+-- 大章
+drop table if exists `chapter`;
+create table `chapter` (
+  `id` char(8) not null comment 'id',
+  `course_id` char(8) comment '课程id',
+  `name` varchar(50) comment '名称',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
+
+
+
+
+
+
+
+
 drop table  if exists `user`;
 create table `test`(
     `id` char(8) not null default '' comment 'id',
