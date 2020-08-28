5de2a26cfdcddcf5d64d863081da497f254b3893
zhaochen0623 <429702531@qq.com>
2020-08-28 15:35:14 +0800

    1.集成分页插件pagehelper,注意页码是从1开始

--- common/src/main/java/com/course/common/service/ChapterServiceImpl.java
+++ common/src/main/java/com/course/common/service/ChapterServiceImpl.java
@@ -4,6 +4,7 @@ import com.course.common.dto.ChapterDto;
 import com.course.common.entity.Chapter;
 import com.course.common.entity.ChapterExample;
 import com.course.common.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,13 +27,14 @@ public class ChapterServiceImpl implements ChapterService {
 
     @Override
     public List<ChapterDto> selectByExample() {
+        PageHelper.startPage(1,1);
         ChapterExample chapterExample = new ChapterExample();
 //        chapterExample.createCriteria().andIdEqualTo("");
 //        chapterExample.setOrderByClause("id desc");
 //
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         List<ChapterDto> chapterDtos = new ArrayList<ChapterDto>();
-        for (int i = 0,l=chapterList.size(); i < 1;  i++){
+        for (int i = 0,l=chapterList.size(); i < l;  i++){
             Chapter chapter=chapterList.get(i);
             ChapterDto chapterDto=new ChapterDto();
             BeanUtils.copyProperties(chapter,chapterDto);
